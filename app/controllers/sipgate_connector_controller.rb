a638f460a04af7425020ae4d9efe4518f42c8ba7
Florian Eck <florian.eck@el-digital.de>
2017-11-26 11:49:23 +0100

    added hook for user settings

--- app/controllers/sipgate_connector_controller.rb
+++ app/controllers/sipgate_connector_controller.rb
@@ -1,12 +1,26 @@
 class SipgateConnectorController < ApplicationController
   
+  before_filter :require_login
+  
   def auth
+    redirect_to rusip_auth.auth_redirect_url
   end
   
   def callback
     if params[:code]
       
     end    
+  end
+  
+  private 
+  
+  def rusip_auth
+    @_rusip_auth ||= RuSip::Auth.new(
+      EasyredmineSipgateConnector.client_id,
+      EasyredmineSipgateConnector.client_secret,
+      EasyredmineSipgateConnector.redirect_url,
+      EasyredmineSipgateConnector.scope
+    )
   end
   
 end
--- lib/easyredmine_sipgate_connector.rb
+++ lib/easyredmine_sipgate_connector.rb
@@ -5,19 +5,39 @@ module EasyredmineSipgateConnector
   CONFIG_FILE_PATH         = File.expand_path("../../config/easyredmine_sipgate_connector.yml", __FILE__)
   
   cattr_reader :yaml_config
-  
-  def config_from_yaml
-    return self.yaml_config if self.yaml_config.present?
+  class << self
+    def config_from_yaml
+      return self.yaml_config if self.yaml_config.present?
 
-    if File.exists?(CONFIG_FILE_PATH)
-      @@yaml_config = YAML::load(File.open(CONFIG_FILE_PATH).read)
-    else
-      raise LoadError, "Cant find config file under #{CONFIG_FILE_PATH}"
+      if File.exists?(CONFIG_FILE_PATH)
+        @@yaml_config = YAML::load(File.open(CONFIG_FILE_PATH).read)
+      else
+        raise LoadError, "Cant find config file under #{CONFIG_FILE_PATH}"
+      end
+    end
+  
+    def scope
+      'all'
+    end
+  
+    def client_id
+      config_from_yaml['client_id']
+    end
+  
+    def client_secrect
+      config_from_yaml['client_secret']
+    end
+  
+    def redirect_url
+      Rails.application.routes.url_helpers.sipgate_callback_url(host: config_from_yaml['redirect_host'])
     end
   end
   
-  def scope
-    'all'
+  class Hooks < Redmine::Hook::ViewListener
+    
+    def helper_user_settings_tabs(context = {})
+      context[:tabs] << {:name => 'sipgate', :partial => 'sipgate_connector/user_settings', :label => :label_sipgate, :user => context[:user]}
+    end
   end
       
 end
