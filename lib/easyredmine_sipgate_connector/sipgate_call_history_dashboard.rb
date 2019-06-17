fd25d1ca741d37c5ea368570c0b9bdc823b3bd47
Florian Eck <florian.eck@el-digital.de>
2019-06-17 22:19:34 +0200

    fix nil error when no devices are set

--- lib/easyredmine_sipgate_connector/sipgate_call_history_dashboard.rb
+++ lib/easyredmine_sipgate_connector/sipgate_call_history_dashboard.rb
@@ -5,21 +5,20 @@ class SipgateCallHistoryDashboard < EasyPageModule
     @category_name ||= 'users'
   end
 
-  def runtime_permissions(  user)
+  def runtime_permissions(user)
     user.sipgate_active?
   end
-  
+
   def show_path
     'sipgate_connector/easy_page_modules/call_history_dashboard_show'
   end
-  
+
   def edit_path
     'sipgate_connector/easy_page_modules/call_history_dashboard_edit'
   end
-  
+
   def get_show_data(settings, user, page_context = {})
-    
-    {entries: SipgateCallHistory.where(user_id: user.id).limit(settings['page_size'] || 10) }
+    { entries: SipgateCallHistory.where(user_id: user.id).limit(settings['page_size'] || 10) }
   end
 
 end
--- lib/easyredmine_sipgate_connector/user_extension.rb
+++ lib/easyredmine_sipgate_connector/user_extension.rb
@@ -1,38 +1,38 @@
 module EasyredmineSipgateConnector
   module UserExtension
-  
+
     extend ActiveSupport::Concern
-    
+
     included do
       serialize :sipgate_devices
       safe_attributes :sipgate_default_device_id, :sipgate_delete_after_fetch
     end
-    
+
     def reload_sipgate_devices
       return false if sipgate_token.nil?
       data = rusip_api.devices_for_user(self.sipgate_user_id)
       self.sipgate_devices = {}
       data['items'].map do |item|
-        self.sipgate_devices[item['id']] = item['alias'] 
+        self.sipgate_devices[item['id']] = item['alias']
       end
-      
+
       if self.sipgate_default_device_id.present? && self.sipgate_devices[self.sipgate_default_device_id].nil? || self.sipgate_devices.size == 1
         self.sipgate_default_device_id = self.sipgate_devices.keys.first
       end
-        
+
     end
-    
+
     def sipgate_active?
-      self.sipgate_user_id.present? && self.sipgate_token.present? && self.sipgate_devices.any?
+      self.sipgate_user_id.present? && self.sipgate_token.present? && (self.sipgate_devices && self.sipgate_devices.any?)
     end
-    
+
     def make_call(deviceid, callee)
       rusip_api.calls(deviceid, callee)
     end
-    
+
     def rusip_api
       @_rusip_api ||= RuSip::Api.new(self.sipgate_token)
     end
-    
+
   end
-end  
+end
